0ec8235746de96f9093d92e529442abb9403d2d3
Ankit Goel <ankgoel@ucdavis.edu>
2024-02-18 20:17:43 -0800

    fixed ORL code

--- code/stage_3_code/Face_Net.py
+++ code/stage_3_code/Face_Net.py
@@ -47,19 +47,18 @@ class Face_Net(method, nn.Module):
         self.pool = nn.MaxPool2d(Kernel_size=2, stride=2)
         self.fc1 = nn.Linear(32 * 56 * 46 , 64)
         self.fc2 = nn.Linear(64, 40)
-        #self.dropout = nn.Dropout(0.3)
+        self.dropout = nn.Dropout(0.2)
 
     # it defines the forward propagation function for input x
     # this function will calculate the output layer by layer
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x))) # size 128
-        x = self.pool(F.relu(self.conv2(x))) # size 64
-        x = self.pool(F.relu(self.conv3(x))) # size 32
-        x = x.view(-1, 64 * 32 * 32 )
-        x = self.dropout(x)
+        x = self.pool(F.relu(self.conv1(x))) 
+        x = self.pool(F.relu(self.conv2(x))) 
+        x = x.view(-1, 32 * 56 * 46)
+        #x = self.dropout(x)
         x = F.relu(self.fc1(x))
-        x = self.dropout(x)
+        #x = self.dropout(x)
         x = self.fc2(x)
         return F.log_softmax(x, dim=1)
 
